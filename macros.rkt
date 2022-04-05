5c8bfe235aee018e2ea5f4b8d03a008dbba34627
Sonja Gurtner <sonja.gurtner@gmail.com>
2022-04-05 15:37:16 +0200

    added and, or

--- macros.rkt
+++ macros.rkt
@@ -11,7 +11,7 @@
 ; parameters for the verifications
 (define-symbolic r1 r2 int32?)
 
-;==============ADD
+;============== ADD
 (define (myadd r1 r2)
   (bvsub r1 (bvsub (int32 0) r2)))
 
@@ -20,7 +20,7 @@
 
 ;============== RECURSIVE / LOOPS
 
-; Parameter that controls the number of unrollings
+; Parameter that controls the number of unrollings, for recurive/loop functions
 (define fuel (make-parameter 32))
 
 (define-syntax-rule
@@ -30,6 +30,7 @@
     (parameterize ([fuel (sub1 (fuel))])
       body ...)))
 
+;example, can be removed
 (define-bounded (bvsqrt n)
   (cond
     [(bvult n (int32 2)) n]
@@ -38,7 +39,7 @@
      (define s1 (bvadd s0 (int32 1)))
      (if (bvugt (bvmul s1 s1) n) s0 s1)]))
 
-;==============SLL
+;============== SLL
 (define-bounded (mysll-safe r1 r2)
   (cond
     [(bveq r2 (int32 0)) r1]
@@ -50,13 +51,14 @@
   (mysll-safe r1 (bvand r2 (int32 31))))
   
 (display "MYSLL\n")
+;bvshl does not take lower 5 bits as riscv sll does, therefore bvand 31
 (verify (assert (eq? (bvshl r1 (bvand r2 (int32 31))) (mysll r1 r2))))
 (verify (begin
           (assume (bvsge r2 (int32 0)))
           (assume (bvslt r2 (int32 32)))
           (assert (eq? (bvshl r1 r2) (mysll r1 r2)))))
 
-;===============XOR
+;============== XOR
 (define (myxor r1 r2)
   (let ([x r1])
     (define rd (int32 0))
@@ -68,4 +70,34 @@
 
 (display "MYXOR\n")
 (verify (assert (eq? (bvxor r1 r2) (myxor r1 r2))))
-  
+
+;============== AND
+(define (myand r1 r2)
+  (let ([x r1])
+    (define rd (int32 0))
+    (for ([i 32])
+      (define ex1 (bvshl (zero-extend (bit i r1) (bitvector 32)) (int32 i)))
+      (define ex2 (bvshl (zero-extend (bit i r2) (bitvector 32)) (int32 i)))
+      (set! rd (bvadd rd (bvand ex1 ex2))))
+     rd))
+
+(display "MYAND\n")
+(verify (assert (eq? (bvand r1 r2) (myand r1 r2))))
+
+
+;============== OR
+(define (myor r1 r2)
+  (let ([x r1])
+    (define rd (int32 0))
+    (for ([i 32])
+      (define ex1 (bvshl (zero-extend (bit i r1) (bitvector 32)) (int32 i)))
+      (define ex2 (bvshl (zero-extend (bit i r2) (bitvector 32)) (int32 i)))
+      (set! rd (bvadd rd (bvor ex1 ex2))))
+     rd))
+
+(display "MYOR\n")
+(verify (assert (eq? (bvor r1 r2) (myor r1 r2))))
+
+;============== SLT
+;============== SLTU
+;============== Branching?
