5f815c909a740df151fcc83d6ab4e95244ecd2ee
Sonja Gurtner <sonja.gurtner@gmail.com>
2022-06-07 13:52:51 +0200

    improved script, fixed closing output file

--- benchmark-verifications.bash
+++ benchmark-verifications.bash
@@ -2,9 +2,13 @@
 
 FILE=riscv-oisc-verifications.rkt
 INTERPRETER=riscv-oisc-interpreter.rkt
-BENCHMARKDIR=/home/sonja/GitHub/riscv-oisc-verifier/benchmarks
+BENCHMARKDIR=/home/sonja/riscv-oisc-verifier/benchmarks
+CVC5=/home/sonja/GitHub/cvc5/build/bin/cvc5
+CVC4=/home/sonja/GitHub/cvc4/build/bin/cvc4
+BITWUZLA=/home/sonja/GitHub/bitwuzla/build/bin/bitwuzla
+BOOLECTOR=/home/sonja/GitHub/boolector-3.2.2/build/bin/boolector
 
-for solver in  cvc4-kissat cvc4-cadical cvc4-cryptominisat \
+for solver in cvc4-kissat cvc4-cadical cvc4-cryptominisat \
 			   boolector-cadical boolector-lingeling boolector-cryptominisat \
 			   z3 \
 			   cvc5-cadical cvc5-kissat cvc5-cryptominisat \
@@ -17,81 +21,81 @@ do
     mkdir -p ${BENCHMARKDIR}/${solver}
 
     sed -i "s|(current-solver (.*))$|(current-solver (${solver}))|g" ${FILE}
-    sed -i "s|/benchmarks/.*/~a.dat|/benchmarks/${solver}/~a.dat|g" ${FILE}
+    sed -i "s|/benchmarks/.*~a.dat|/benchmarks/${solver}/~a.dat|g" ${FILE}
 
 
     # CVC5
     if [ ${solver} == "cvc5-cadical" ];
     then
-        sed -i "s|(current-solver (cvc5.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc5/build/bin/cvc5\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cadical)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC5}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cadical)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "cvc5-kissat" ];
     then
-        sed -i "s|(current-solver (cvc5.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc5/build/bin/cvc5\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'kissat)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC5}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'kissat)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "cvc5-cryptominisat" ];
     then
-        sed -i "s|(current-solver (cvc5.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc5/build/bin/cvc5\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC5}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
     fi;
 
     #CVC4
     if [ ${solver} == "cvc4-cadical" ];
     then
-        sed -i "s|(current-solver (cvc4.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc4/build/bin/cvc4\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cadical)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC4}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cadical)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "cvc4-cryptominisat" ];
     then
-        sed -i "s|(current-solver (cvc4.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc4/build/bin/cvc4\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC4}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "cvc4-kissat" ];
     then
-        sed -i "s|(current-solver (cvc4.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc4/build/bin/cvc4\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'kissat)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC4}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'kissat)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "cvc4-cryptominisat" ];
     then
-        sed -i "s|(current-solver (cvc4.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"/home/sonja/GitHub/cvc4/build/bin/cvc4\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (cvc4 #:logic \"QF_BV\" #:path \"${CVC4}\" #:options (hash ':bitblast 'eager ':bv-sat-solver 'cryptominisat)))|g" ${FILE}
     fi;
 
     # Bitwuzla
     if [ ${solver} == "bitwuzla-lingeling" ];
     then
-        sed -i "s|(current-solver (cvc5.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/bitwuzla/build/bin/bitwuzla\" #:options (hash ':sat-engine '0)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BITWUZLA}\" #:options (hash ':sat-engine '0)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "bitwuzla-cadical" ];
     then
-        sed -i "s|(current-solver (bitwuzla.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/bitwuzla/build/bin/bitwuzla\" #:options (hash ':sat-engine '1)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BITWUZLA}\" #:options (hash ':sat-engine '1)))|g" ${FILE}
     fi;
 
 	if [ ${solver} == "bitwuzla-kissat" ];
     then
-        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/bitwuzla/build/bin/bitwuzla\" #:options (hash ':sat-engine '2)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BITWUZLA}\" #:options (hash ':sat-engine '2)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "bitwuzla-cryptominisat" ];
     then
-        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/bitwuzla/build/bin/bitwuzla\" #:options (hash ':sat-engine '5)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BITWUZLA}\" #:options (hash ':sat-engine '5)))|g" ${FILE}
     fi;
 
     #boolector
     if [ ${solver} == "boolector-cadical" ];
     then
-        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/boolector-3.2.2/build/bin/boolector\" #:options (hash ':sat-engine '1)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BOOLECTOR}\" #:options (hash ':sat-engine '1)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "boolector-lingeling" ];
     then
-        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/boolector-3.2.2/build/bin/boolector\" #:options (hash ':sat-engine '0)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BOOLECTOR}\" #:options (hash ':sat-engine '0)))|g" ${FILE}
     fi;
 
     if [ ${solver} == "boolector-cryptominisat" ];
     then
-        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"/home/sonja/GitHub/boolector-3.2.2/build/bin/boolector\" #:options (hash ':sat-engine '4)))|g" ${FILE}
+        sed -i "s|(current-solver (.*))$|(current-solver (boolector #:path \"${BOOLECTOR}\" #:options (hash ':sat-engine '4)))|g" ${FILE}
     fi;
 
     for i in 8 12 16 20 24 28 32
